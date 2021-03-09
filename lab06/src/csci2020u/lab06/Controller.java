10efb5888e361a0a27e652e90a2873e12b257141
RomuloVPartoriza <partoriza.romulojr@gmail.com>
2021-03-09 15:24:00 -0500

    Lab06 - Modified syntax: UI still same

--- lab06/src/csci2020u/lab06/Controller.java
+++ lab06/src/csci2020u/lab06/Controller.java
@@ -4,42 +4,40 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 
 
 public class Controller {
     @FXML private Canvas canvas;
-    @FXML private GraphicsContext gc;
-
+    
     //red
-    private static double[] avgHousingPricesByYear = {
+    final private static double[] avgHousingPricesByYear = {
             247381.0,264171.4,287715.3,294736.1,
             308431.4,322635.9,340253.0,363153.7
     };
 
     //blue
-    private static double[] avgCommercialPricesByYear = {
+    final private static double[] avgCommercialPricesByYear = {
             1121585.3,1219479.5,1246354.2,1295364.8,
             1335932.6,1472362.0,1583521.9,1613246.3
     };
 
-    private static String[] ageGroups = {
+    final private static String[] ageGroups = {
         "18-25", "26-35", "36-45", "46-55", "56-65", "65+"
     };
 
-    private static int[] purchasesByAgeGroup = {
+    final private static int[] purchasesByAgeGroup = {
         648, 1021, 2453, 3173, 1868, 2247
     };
 
-    private static Color[] pieColours = {
+    final private static Color[] pieColours = {
         Color.AQUA, Color.GOLD, Color.DARKORANGE,
         Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM
     };
 
     @FXML
     public void initialize(){
-        gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
         drawGraph(gc);
         drawPieChart(gc);
@@ -64,10 +62,10 @@ public class Controller {
     }
 
     public void drawGraph(GraphicsContext gc){
-        int x = 120;
-        int y = 400;
+        double x = 120.0;
+        double y = 400.0;
         double xInc = (x/avgHousingPricesByYear.length);
-        double width = 50;
+        double width = 50.0;
         double maxVal = Double.NEGATIVE_INFINITY;
         double minVal = Double.MAX_VALUE;
 
